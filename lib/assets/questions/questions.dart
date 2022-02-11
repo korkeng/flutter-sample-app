1c891a7fbfb642836e7080260e117708e32dfb01
chawanan.akkarapatompong <chawanan.akkarapatompong@scbtechx.io>
2022-02-11 23:05:21 +0700

    Added sample question for pre and post tests

---
+++ lib/assets/questions/questions.dart
@@ -0,0 +1,46 @@
+class QuestionModel {
+  late final int? no;
+  late final String? question;
+  late final List<String>? choise;
+
+  QuestionModel({
+    this.no,
+    this.question,
+    this.choise,
+  });
+}
+
+class Question {
+  static final getQuestionList = [
+    QuestionModel(
+      no: 1,
+      question: '1 + 1 = ?',
+      choise: [
+        '1',
+        '2',
+        '3',
+        '4',
+      ],
+    ),
+    QuestionModel(
+      no: 2,
+      question: 'ตัวอย่างคำถาม ตัวอย่างคำถาม ตัวอย่างคำถาม ตัวอย่างคำถาม ?',
+      choise: [
+        'ตัวอย่างตัวเลือก 1',
+        'ตัวอย่างตัวเลือก 2 ตัวอย่างตัวเลือก 2',
+        'ตัวอย่างตัวเลือก 3 ตัวอย่างตัวเลือก 3 ตัวอย่างตัวเลือก 3',
+        'ตัวอย่างตัวเลือก 4 ตัวอย่างตัวเลือก 4 ตัวอย่างตัวเลือก 4 ตัวอย่างตัวเลือก 4',
+      ],
+    ),
+    QuestionModel(
+      no: 3,
+      question: '3 + 4 = ?',
+      choise: [
+        '5',
+        '6',
+        '7',
+        '8',
+      ],
+    ),
+  ];
+}
