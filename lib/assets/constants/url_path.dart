d553b866d05454397ad4da96a8ac60b29d63dd14
keng-chawanan <keng@appsynth.net>
2023-04-11 00:45:59 +0700

    Add survey form and webview

---
+++ lib/assets/constants/url_path.dart
@@ -0,0 +1,4 @@
+class URLPath {
+  static const String surveyLink =
+      'https://docs.google.com/forms/d/e/1FAIpQLSfPrZ9s87MYnFAfBo9sAOndlikxI8O_9nVrc2FyXV5ykdP9FA/viewform';
+}
--- lib/src/pages/survey_page.dart
+++ lib/src/pages/survey_page.dart
@@ -1,4 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:sample_app/assets/constants/translation_keys.dart';
+import 'package:sample_app/assets/constants/url_path.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class SurveyPage extends StatefulWidget {
   SurveyPage({Key? key}) : super(key: key);
@@ -8,8 +12,39 @@ class SurveyPage extends StatefulWidget {
 }
 
 class _SurveyPageState extends State<SurveyPage> {
+  bool isLoading = true;
+
+  var controller = WebViewController();
+
+  @override
+  void initState() {
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) => NavigationDecision.navigate,
+        ),
+      )
+      ..loadRequest(Uri.parse(URLPath.surveyLink));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text(tr(TranslationKey.satisfactionSurvey))),
+      body: isLoading ? Center(child: CircularProgressIndicator()) : WebViewWidget(controller: controller),
+    );
   }
 }
